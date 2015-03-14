acb14d1310a8e7093e887a10333f1bb2f0b7062a
evanroane <evan.roane@gmail.com>
2015-03-14 12:36:24 -0500

    added context.cs to repo

---
+++ BAT/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BAT.Models
+{
+    public class BATDbContext : DbContext
+    {
+        public DbSet<BehaviorEvent> BehaviorEvents { get; set; }
+        public DbSet<Input> Inputs { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<CodeSet> CodeSets { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<CodeSetPermission> CodeSetPermissions { get; set; }
+    }
+}
