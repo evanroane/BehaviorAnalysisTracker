4059b13c84ccb4cbec9205963611e5d5abb4f32c
evanroane <evan.roane@gmail.com>
2015-03-19 13:29:22 -0500

    added session permission table

---
+++ BAT/Models/SessionPermission.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BAT.Models
+{
+    public class SessionPermission
+    {
+        public SessionPermission(string ownerId, string participantID, int sessionID)
+        {
+            this.OwnerID = ownerId;
+            this.ParticipantID = participantID;
+            this.SessionID = sessionID;
+        }
+        
+        public SessionPermission() { }
+
+        [Required]
+        public string OwnerID {get; set;}
+
+        [Required]
+        public int SessionID {get; set;}
+
+        [Required]
+        public string ParticipantID { get; set; }
+    }
+}
