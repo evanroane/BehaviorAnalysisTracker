297bf7a2ec2bca3ada809edd00c9540d17001c26
evanroane <evan.roane@gmail.com>
2015-03-21 11:03:30 -0500

    removed 'participants' class from model

--- BAT/Models/Context.cs
+++ BAT/Models/Context.cs
@@ -10,7 +10,6 @@ namespace BAT.Models
     {
         public DbSet<BehaviorEvent> BehaviorEvents { get; set; }
         public DbSet<Input> Inputs { get; set; }
-        public DbSet<Participant> Participants { get; set; }
         public DbSet<CodeSet> CodeSets { get; set; }
         public DbSet<Session> Sessions { get; set; }
         public DbSet<CodeSetPermission> CodeSetPermissions { get; set; }
