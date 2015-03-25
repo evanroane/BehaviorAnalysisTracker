13492cbb5f6b78a289f9522fd81ea933f8a91774
evanroane <evan.roane@gmail.com>
2015-03-25 12:47:02 -0500

    users seed add

--- BAT/Migrations/Configuration.cs
+++ BAT/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace BAT.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using BAT.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BAT.Models.BATDbContext>
     {
@@ -13,56 +15,49 @@ namespace BAT.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+
+        private void AddUsers(ApplicationDbContext context)
+        {
+            if (!(context.Users.Any(u => u.UserName == "evan.roane@gmail.com")))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userToInsert = new ApplicationUser { UserName = "evan.roane@gmail.com", PhoneNumber = "0123456789" };
+                var secondUserToInsert = new ApplicationUser { UserName = "jessie.torelli@gmail.com", PhoneNumber = "9876543210" };
+                userManager.Create(userToInsert, "password");
+                userManager.Create(secondUserToInsert, "password");
+            }
+            context.SaveChanges();
+        }
+
+
         protected override void Seed(BAT.Models.BATDbContext context)
         {
             {
-  "codeSetId" : "Classroom Behavior",
-  "description" : "Designed to observe a targeted student's behavior during classroom instruction",
-  "inputs" : [ {
-    "color" : "btn-primary",
-    "name" : "Praise"
-  }, {
-    "color" : "btn-primary",
-    "name" : "Reprimand"
-  }, {
-    "color" : "btn-primary",
-    "name" : "OTR"
-  }, {
-    "color" : "btn-primary",
-    "name" : "Response"
-  }, {
-    "color" : "btn-primary",
-    "name" : "Nonresponse"
-  }, {
-    "color" : "btn-primary",
-    "name" : "Verbal_disruptive"
-  }, {
-    "color" : "btn-primary",
-    "name" : "Phys_disruptive"
-  }
-            
-            
-            
-            //context.CodeSets;
-            context.Inputs.AddOrUpdate<Input>(i => i.InputID,
-            new Input
-                {
-                    InputName = "aggression",
-                    InputType = "event",
-                    InputColor = "btn-default" 
-                }
+
+                context.CodeSets.AddOrUpdate<CodeSet>(i => i.CodeSetID,
+                    new CodeSet
+                    { 
+                        CodeSetOwner = ""    
+                    }
+
+                );
+ 
+                context.Inputs.AddOrUpdate<Input>(i => i.InputID,
+                new Input
+                    {
+                        InputName = "aggression",
+                        InputType = "event",
+                        InputColor = "btn-default"
+                    }
+                );
+                //context.CodeSetPermissions;
+                //context.Sessions;
+                //context.SessionPermissions;
+                //context.BehaviorEvents;
 
 
-
-
-
-            );
-            //context.CodeSetPermissions;
-            //context.Sessions;
-            //context.SessionPermissions;
-            //context.BehaviorEvents;
-            
-            
+            }
         }
     }
 }
