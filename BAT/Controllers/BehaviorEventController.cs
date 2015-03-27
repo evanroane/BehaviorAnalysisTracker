bf482959903c17efcc0e3eed94be9ae3f0eb4289
evanroane <evan.roane@gmail.com>
2015-03-26 21:32:01 -0500

    behavior event get working

---
+++ BAT/Controllers/BehaviorEventController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BAT.Models;
+using BAT.Repository;
+using Microsoft.AspNet.Identity;
+
+namespace BAT.Controllers
+{
+    public class BehaviorEventController : ApiController
+    {
+        private static BATRepository _db = new BATRepository();
+
+        [HttpGet]
+        [Route("api/behaviorevent/{sessionID}")]
+        public System.Web.Mvc.JsonResult GetSessions(int sessionID)
+        {
+            var behaviorEvents = _db.GetEventsBySessionID(sessionID);
+            var json = new System.Web.Mvc.JsonResult();
+            json.Data = new
+            {
+                behaviorEvents
+            };
+            return json;
+        }
+    }
+}
