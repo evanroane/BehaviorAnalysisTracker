4e4f874a188254e10f38c07bb76f9cec0cb0c697
evanroane <evan.roane@gmail.com>
2015-03-26 19:01:58 -0500

    ng show and hide working with login bool

--- BAT/Scripts/App/main.controller.js
+++ BAT/Scripts/App/main.controller.js
@@ -1,9 +1,7 @@
 ﻿; (function () {
     'use strict';
     angular.module('batApp')
-      .controller('MainController', function ($http, $rootScope, $scope) {
-          alert("controller");
-          
+      .controller('MainController', function ($http, $rootScope, $scope) {          
           $rootScope.$on('$routeChangeStart', function () {
               $rootScope.loading = true;
           });
@@ -20,7 +18,6 @@
                     $rootScope.isLoggedIn = true;
                     $rootScope.userID = result.data;
                     console.log($rootScope.userID);
-                    alert('aaaaa');
                 } else {
                     $rootScope.isLoggedIn = false;
                 }
